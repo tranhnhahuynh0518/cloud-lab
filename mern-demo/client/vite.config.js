c37ff550bbf7b89a399843c7bc9cbb1ac804acca
trannhahuynh <trannhahuynhcm123@gmail.com>
2026-09-10 23:11:27 +0700

    Complete Lab 03 Docker MERN

--- mern-demo/client/vite.config.js
+++ mern-demo/client/vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://mern-backend:5000',
         changeOrigin: true,
       }
     }
